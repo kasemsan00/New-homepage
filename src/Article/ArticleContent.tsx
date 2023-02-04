e6dff471efb1b2d92d11cb335ec070e13889ede9
kasemsan00 <kasemsan.cho@gmail.com>
2023-02-04 18:51:12 +0700

    .

--- src/Article/ArticleContent.tsx
+++ src/Article/ArticleContent.tsx
@@ -7,19 +7,19 @@ export default function ArticleContent() {
                 imagePath={"../../images/image-retro-pcs.jpg"}
                 number={"01"}
                 title={"Reviving Retro PCs"}
-                content={"What happens when old PCs are given modern upgrades"}
+                content={"What happens when old PCs are given modern upgrades?"}
             />
             <ArticleItem
                 imagePath={"../../images/image-top-laptops.jpg"}
                 number={"02"}
                 title={"Top 10 Laptops of 2022"}
-                content={"Our best picks for various needs and budgets"}
+                content={"Our best picks for various needs and budgets."}
             />
             <ArticleItem
                 imagePath={"../../images/image-gaming-growth.jpg"}
                 number={"03"}
                 title={"The Growth of Gaming"}
-                content={"How the pandemic has sparked fresh opportunities"}
+                content={"How the pandemic has sparked fresh opportunities."}
             />
         </div>
     );
